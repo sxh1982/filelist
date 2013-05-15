9ad4740255d9b2f3f12516e8c74857e531ec7d0c
unknown <Administrator@.(none)>
2013-05-15 14:28:13 +0800

    g

--- js/plugins.js
+++ js/plugins.js
@@ -4,7 +4,7 @@
 window.log = function(){
   log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
-  if(this.console) {
+  if(this.console) {fff
     arguments.callee = arguments.callee.caller;
     var newarr = [].slice.call(arguments);
     (typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
